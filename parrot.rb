47fd12ba30ae4eb570d7c57823959bc72cbb2a62
Jung Lee <lee.jung.s95@gmail.com>
2020-04-16 04:48:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(*p)
-  puts "Squawk!"|| p
-  return "Squawk!" || p
-end
+
 
 def parrot(a = "Squawk!")
   if parrot() == "Squawk!"
